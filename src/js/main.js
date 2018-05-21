1561efb1ba26c8c4ef6b9bd8b0da54dfb334de4b
Ricardo Goldstein <ricardo.goldstein@zee-dog.com>
2018-05-20 23:49:43 -0300

    add jquery

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,11 @@
   menu.addEventListener('click', function () {
     menu.classList.toggle('opened');
   });
+  $(".js-rsvp").submit(function(e) {
+    e.preventDefault();
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      alert("Thank you!");
+    });
+  });
 }());
