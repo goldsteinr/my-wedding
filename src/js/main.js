9dd6d6b636b6480496e95f183e2149d30fbc3948
Ricardo Goldstein <ricardo.goldstein@zee-dog.com>
2018-06-05 22:14:17 -0300

    add form

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,34 @@
 (function () {
   var menu = document.querySelector('.js-menu');
+  var selectGuests = document.querySelector('.js-guests');
   menu.addEventListener('click', function () {
     menu.classList.toggle('opened');
   });
-  $(".js-rsvp").submit(function(e) {
+  selectGuests.addEventListener('change', function(e) {
+    var numberGuests = parseInt(selectGuests.value);
+    var guests = document.querySelectorAll('.guest');
+    for (var i = 0; i < guests.length; i += 1) {
+      guests[i].classList.remove('show');
+      guests[i].querySelector('input').required = false;
+    }
+    for (var i = 0; i < numberGuests; i += 1) {
+      var guestInput = document.querySelector('.guest-' + (i+1));
+      guestInput.classList.add('show');
+      guestInput.querySelector('input').required = true;
+    }
+  });
+  document.querySelector(".js-rsvp").addEventListener('submit', function(e) {
     e.preventDefault();
     var $form = $(this);
     $.post($form.attr("action"), $form.serialize()).then(function() {
-      alert("Thank you!");
+      swal({
+        type: 'success',
+        title: 'Obrigado!',
+        text: 'Sua presença foi confirmada.',
+        showConfirmButton: false,
+        timer: 3000
+      });
+      return document.querySelector(".js-rsvp").reset();
     });
   });
 }());
