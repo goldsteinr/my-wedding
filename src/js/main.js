4914a9b21a6c0069234b837049fe3996ce4659ad
Ricardo Goldstein <ricardo.goldstein@zee-dog.com>
2018-07-12 00:00:22 -0300

    remove guests

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 (function () {
   var menu = document.querySelector('.js-menu');
-  var selectGuests = document.querySelector('.js-guests');
   menu.addEventListener('click', function () {
     menu.classList.toggle('opened');
   });
@@ -9,19 +8,6 @@
   var scroll = new SmoothScroll('a[href*="#"]', {
     offset: 50,
     speed: 800,
-  });
-  selectGuests.addEventListener('change', function(e) {
-    var numberGuests = parseInt(selectGuests.value);
-    var guests = document.querySelectorAll('.guest');
-    for (var i = 0; i < guests.length; i += 1) {
-      guests[i].classList.remove('show');
-      guests[i].querySelector('input').required = false;
-    }
-    for (var i = 0; i < numberGuests; i += 1) {
-      var guestInput = document.querySelector('.guest-' + (i+1));
-      guestInput.classList.add('show');
-      guestInput.querySelector('input').required = true;
-    }
   });
   document.querySelector(".js-rsvp").addEventListener('submit', function(e) {
     e.preventDefault();
